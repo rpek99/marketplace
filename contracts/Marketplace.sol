1cb7ac950e2aff6bb0e47115de9e867c1a30f304
rustem <rstmpk28@gmail.com>
2022-05-27 23:59:12 +0300

    getMyListing and getSpecificListing functions added

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -31,7 +31,7 @@ contract Marketplace {
         owner = payable(msg.sender);
         _governmentAccount = payable(governmentAccount);
     }
-
+    //create new listing
     function createListing(
         address assetContract,
         uint256 tokenId,
@@ -51,10 +51,7 @@ contract Marketplace {
         IERC721(assetContract).transferFrom(msg.sender, address(this), tokenId);
     }
 
-    //TODO
-    //function cancelListing() {}
-    //function updateListing() {} maybe
-
+    // buy/sell listing
     function buy(uint256 listingId) external payable {
         require(msg.sender != address(0), "Address should not be 0");
 
@@ -92,6 +89,7 @@ contract Marketplace {
         );
     }
 
+    // get all listings
     function getListings() external view returns (Listing[] memory) {
         Listing[] memory activeListings = new Listing[](
             totalListings.current()
@@ -103,4 +101,38 @@ contract Marketplace {
 
         return activeListings;
     }
+
+    //get individual listings
+    function getMyListings() public view returns (Listing[] memory) {
+        uint256 totalListingCount = totalListings.current();
+        uint256 listingCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint i = 0; i < totalListingCount; i++) {
+            if (listings[i + 1].owner == msg.sender) {
+                listingCount += 1;
+            }
+        } 
+        Listing[] memory items = new Listing[](listingCount);
+        for (uint i = 0; i < totalListingCount; i++) {
+            if (listings[i + 1].owner == msg.sender) {
+                uint currentId = i + 1;
+                Listing storage currentListing = listings[currentId];
+                items[currentIndex] = currentListing;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    // get specific listing 
+    function getSpecificListing(uint256 listingId) public view returns (Listing memory){
+        return listings[listingId];
+    }
+
+    //TODO
+    //function updateListing() {} maybe
+    //function removeListing() {} maybe
+    //function addListing() {} maybe
 }
