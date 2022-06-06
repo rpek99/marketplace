bd917c0d97fedbfdcfbae547fbe200476819b844
rpek99 <rstmpk28@gmail.com>
2022-06-07 01:50:49 +0300

    deploy.ts file updated

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,13 +13,22 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
+  const GOVERNMENT_ACCOUNT_ADDRESS =
+    "0xEF4Eef3FFdA3E05D347986862726a955353A2227";
+
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Marketplace = await ethers.getContractFactory("Marketplace");
+  const marketplace = await Marketplace.deploy(GOVERNMENT_ACCOUNT_ADDRESS);
 
-  await greeter.deployed();
+  await marketplace.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Marketplace deployed: ", marketplace.address);
+
+  const PropertyNFT = await ethers.getContractFactory("PropertyNFT");
+  const nft = await PropertyNFT.deploy(marketplace.address);
+  await nft.deployed();
+
+  console.log("NFT deployed: ", nft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
